422b49be368952b7674166601dbd02f97e57c7e6
Steffi <steffitan@Steffis-MacBook-Pro-16.local>
2025-06-13 20:18:54 +0800

    Add new project

---
+++ GithubCommunity/GithubCommunity/GithubCommunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCommunityApp.swift
+//  GitHubCommunity
+//
+//  Created by Steffi on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCommunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
